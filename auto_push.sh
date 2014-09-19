003023edc53272ed1f3b5b1483e0aadb747d255d
mabelcalim <mabelcalim@gmail.com>
2014-09-19 20:29:57 -0300

    rm swp

--- auto_push.sh
+++ auto_push.sh
@@ -4,7 +4,7 @@ cd /Users/usuario/git_repos/tide-app/
 git pull
 git status
 git add .
-git commit -am "auto commit and push"
+git commit -am "auto commit and push everyday at 23h"
 git push
 
 exit 0
